71ecc2f57b45f84ff14cf7561099b03b40f86edc
Rian Iregho <rianiregho@gmail.com>
2021-01-28 17:26:29 +0700

    Testing

--- src/App.js
+++ src/App.js
@@ -1,69 +1,61 @@
 import logo from './logo.svg';
-import React, { useState } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import './App.css';
 // or less ideally
 import 'bootstrap/dist/css/bootstrap.css';
-import { Button, Carousel, Container, Row, Col, Form } from 'react-bootstrap';
+import { Button, Carousel, Container, Jumbotron, Table, Row, Col, Form } from 'react-bootstrap';
 
-function App() {
-  const [slide1, setSlide1] = useState('Title Default')
-  const [slide2, setSlide2] = useState('Title Default')
+class App extends React.Component {
+  defaultProps = {
+    min : 97,
+    max : 122
+  }
+  constructor(props) {
+    super(props);
+    this.state = {counter: this.defaultProps.min};
+  }
 
+  plus(){
+    if(this.state.counter < this.defaultProps.max){
+      this.setState({
+        counter: this.state.counter + 1
+      })
+    }
+  }
+  minus(){
+    if(this.state.counter > this.defaultProps.min){
+      this.setState({
+        counter: this.state.counter - 1
+      })
+    }
+  }
+  
 
+  render(){
+    return (
+      <div style={{width:'100%', flex:1, paddingLeft:'20%', alignItems:'center', justifyContent:'center'}}>
+          <Row style={{alignItems:'center', alignContent:'center', justifyContent:'center'}}>
 
-  return (
-    <div style={{width:'100%', flex:1, paddingLeft:'20%', alignItems:'center', justifyContent:'center'}}>
-      <div style={{width:1000, height:500, backgroundPosition:'center', alignContent:'center', marginTop:100}}>
-        <Carousel controls={true}>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://play-lh.googleusercontent.com/proxy/2tj1HTTkxfLUCHMYCMY7Ik_u9Dv-ctrQ7tteluo8MkL9bUzSFutbEcvkGroJxU6PTS84IHjfzCYjRsCflXcZ5k_CV2OAD2Al4i_fUCrb6cBVNvtB4TZhu97Z=w720-h405-rw"
-              alt="First slide"
-              style={{height:500}}
-            />
-            <Carousel.Caption>
-              <h3>{slide1}</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://cdn-2.tstatic.net/banjarmasin/foto/bank/images/minion-the-rise-of-gru-3.jpg"
-              alt="Third slide"
-              style={{height:500}}
-            />
+            {/* Button Minus */}
+            <Col>
+              <Button onClick={() => {this.minus()}}>+</Button>
+            </Col>
 
-            <Carousel.Caption>
-              <h3>{slide2}</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-      </Carousel>
-      <Container style={{marginTop:30}}>
-        <Row>
-          <Col>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Slide 1</Form.Label>
-              <Form.Control type="email" placeholder="Slide 1 Title" onChange={e => {setSlide1(e.target.value)}} />
-              <Form.Text className="text-muted">
-                Type to change Title of Slide 1
-              </Form.Text>
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Slide 2</Form.Label>
-              <Form.Control type="email" placeholder="Slide 2 Title" onChange={e => {setSlide2(e.target.value)}} />
-              <Form.Text className="text-muted">
-                Type to change Title of Slide 2
-              </Form.Text>
-            </Form.Group>
-          </Col>
-        </Row>
-    </Container>
+            {/* Displaying */}
+            <Col xs={5}>
+              <Jumbotron style={{witdh:1000, height:100, backgroundColor:'#fff'}}>
+                <h1 style={{textTransform:'uppercase'}}>{String.fromCharCode(this.state.counter)}</h1>
+              </Jumbotron>
+            </Col>
+
+            {/* Button Minus */}
+            <Col>
+              <Button onClick={() => {this.plus()}}>+</Button>
+            </Col>
+          </Row>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
